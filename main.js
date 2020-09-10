0d167c9678dbe54cfaef8d2526028a58e196b64c
HEIZ2303 <pavel@tolpygo.ru>
2020-09-10 18:53:03 +0300

    third change

--- main.js
+++ main.js
@@ -28,6 +28,8 @@ function getQuantityElements (heightElement) {
 
 function startGame(){
     start.classList.add('hide');
+    gameArea.innerHTML = '';
+    
     
     for (let i = 0; i < getQuantityElements(100); i++){
         const line = document.createElement('div');
@@ -46,9 +48,12 @@ function startGame(){
         enemy.style.background = 'transparent url("./image/enemy2.png") center / cover no-repeat';
         gameArea.appendChild(enemy);
     }
-    
+    setting.score = 0;
     setting.start = true;
     gameArea.appendChild(car);
+    car.style.left = gameArea.offsetWidth / 2 - car.offsetWidth/2;
+    car.style.top = 'auto';
+    car.style.bottom = '10px';
     setting.x = car.offsetLeft;
     setting.y = car.offsetTop;
 
@@ -56,9 +61,11 @@ function startGame(){
 }
 
 function playGame(){
-    moveRoad();
-    moveEnemy();
+    setting.score += setting.speed;
+    score.innerHTML = 'SCORE<br>' + setting.score;
     if(setting.start){
+        moveRoad();
+        moveEnemy();
         if (keys.ArrowLeft && setting.x > 0) {
             setting.x -= setting.speed;
         }
@@ -105,7 +112,21 @@ function moveRoad() {
 
 function moveEnemy() {
      let enemy = document.querySelectorAll('.enemy');
+
      enemy.forEach(function(item) {
+         let carRect = car.getBoundingClientRect();
+         let enemyRect = item.getBoundingClientRect();
+         
+            if (carRect.top <= enemyRect.bottom &&
+                carRect.right>= enemyRect.left &&
+                carRect.left <= enemyRect.right &&
+                carRect.bottom >= enemyRect.top) {
+                    setting.start = false;
+                    console.warn('ДТП');
+                    start.classList.remove('hide');
+                    start.style.top = score.offsetHeight;
+            }
+
         item.y += setting.speed / 2;
         item.style.top = item.y + 'px';
         if(item.y >= document.documentElement.clientHeight) {
